05d1350f6b3aad85df1476b37888f7db7c9a00e3
coder7475 <robiulhossain7475@gmail.com>
2024-11-19 08:38:39 +0600

    feat: learn to response with plain text

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
+from starlette.responses import PlainTextResponse
 from starlette.routing import Route
 
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
-app = Starlette(debug=True, routes=[
-    Route('/', homepage)
-])
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+    response = PlainTextResponse('Hello, Plain!')
+    await response(scope, receive, send)
+
+# app = Starlette(debug=True, routes=[
+#     Route('/', homepage)
+# ])
